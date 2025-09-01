2acb325efcdcb530406ff0b0d38620dc64f0c4d6
PowerAngelXD <692732163@qq.com>
2025-09-01 17:09:17 +0800

    vm设计完成

--- Interpreter/vm/vm.h
+++ Interpreter/vm/vm.h
@@ -2,11 +2,34 @@
 #define SAKORA_VM_H
 
 #include "thread.h"
-#include "../value.h"
+#include "../scope.h"
 #include <stack>
 
 namespace svm {
-    
+    class VMInstance {
+        std::stack<sakora::Value> runtimeStack;
+        threadGroup threadMgr;
+        sakora::ScopeManager scopeMgr;
+        std::vector<std::string> codeArgs; // 当前code的args
+    public:
+        // vm内工具方法
+        inline sakora::Value Pop();
+        void newThread();
+        void removeThread();
+        // 对应的指令实现方法
+        void vmPush();
+        void vmAdd();       void vmSub();       void vmMul();          void vmDiv();
+        void vmLgcAnd();    void LgcOr();       void vmLgcEqu();       void vmLgcNot();
+        void vmLgcMrThan(); void vmLgcLsThan(); void vmLgcMrequThan(); void vmLgcLsequThan();
+        void vmArrMake();   void vmArrTidyChk();
+        void vmDeclare();   void vmAssign();    void vmGet();          void vmFrom();
+        void vmNewScope();  void vmEndScope();
+        void vmJtin();      void vmJtbck();     void vmJout();
+        //
+
+        void start(bool isDebug = false);
+
+    };
 }
 
 #endif
