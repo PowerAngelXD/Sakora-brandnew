b32b08c62c0603e50b0147e9ee582e3147fa62ed
PowerAngelXD <692732163@qq.com>
2025-09-15 23:06:09 +0800

    对visitor的设计完成

--- Interpreter/visitor.h
+++ Interpreter/visitor.h
@@ -42,7 +42,125 @@ namespace sakora {
 
         void make(VMCode code);
 
-        
+        /**
+     * 生成函数调用相关指令
+     * Visit instructions for function call
+     */
+    void visit(AST::BasicIdentifierNode node, bool pushObj = false);
+    /**
+     * 生成原子标识符相关指令
+     * Visit instructions for atomic identifier
+     */
+    void visit(AST::AtomIdentifierNode node, bool pushObj = false);
+    /**
+     * 生成标识符表达式相关指令
+     * Visit instructions for identifier expression
+     */
+    void visit(AST::IdentifierExprNode node, bool pushObj = false);
+    /**
+     * 生成基本表达式相关指令
+     * Visit instructions for primary expression
+     */
+    void visit(AST::PrimExprNode node);
+    /**
+     * 生成乘法表达式相关指令
+     * Visit instructions for multiplication expression
+     */
+    void visit(AST::MulExprNode node);
+    /**
+     * 生成加法表达式相关指令
+     * Visit instructions for addition expression
+     */
+    void visit(AST::AddExprNode node);
+    /**
+     * 生成逻辑表达式相关指令
+     * Visit instructions for logic expression
+     */
+    void visit(AST::LogicExprNode node);
+    /**
+     * 生成布尔表达式相关指令
+     * Visit instructions for boolean expression
+     */
+    void visit(AST::BoolExprNode node);
+    /**
+     * 生成基本类型表达式相关指令
+     * Visit instructions for primary type expression
+     */
+    void visit(AST::PrimTypeExprNode node);
+    /**
+     * 生成数组类型表达式相关指令
+     * Visit instructions for array type expression
+     */
+    void visit(AST::ArrayTypeExprNode node);
+    /**
+     * 生成类型表达式相关指令
+     * Visit instructions for type expression
+     */
+    void visit(AST::TypeExprNode node);
+    /**
+     * 生成数组表达式相关指令
+     * Visit instructions for array expression
+     */
+    void visit(AST::ArrayExprNode node);
+    /**
+     * 生成完整表达式相关指令
+     * Visit instructions for whole expression
+     */
+    void visit(AST::WholeExprNode node);
+
+    // Statement 语句相关 / Statement related
+    /**
+     * 生成let声明语句相关指令
+     * Visit instructions for let statement
+     */
+    void visit(AST::LetStmtNode node);
+    /**
+     * 生成赋值语句相关指令
+     * Visit instructions for assignment statement
+     */
+    void visit(AST::AssignStmtNode node);
+    /**
+     * 生成代码块语句相关指令
+     * Visit instructions for block statement
+     * @param jmpCond 是否为条件跳转块 / Whether it is a conditional jump block
+     */
+    void visit(AST::BlockStmtNode node, bool jmpCond = false);
+    /**
+     * 生成if条件语句相关指令
+     * Visit instructions for if statement
+     */
+    void visit(AST::IfStmtNode node);
+    /**
+     * 生成else if语句相关指令
+     * Visit instructions for else-if statement
+     */
+    void visit(AST::ElseIfStmtNode node);
+    /**
+     * 生成else语句相关指令
+     * Visit instructions for else statement
+     */
+    void visit(AST::ElseStmtNode node);
+    /**
+     * 生成match匹配语句相关指令
+     * Visit instructions for match statement
+     */
+    void visit(AST::MatchStmtNode node);
+    /**
+     * 生成while循环语句相关指令
+     * Visit instructions for while statement
+     */
+    void visit(AST::WhileStmtNode node);
+    /**
+     * 生成表达式语句相关指令
+     * Visit instructions for expression statement
+     */
+    void visit(AST::ExprStmtNode node);
+
+    /**
+     * 生成通用语句相关指令
+     * Visit instructions for general statement
+     */
+    void visit(AST::StmtNode node);
     };
 }
 
